cc2dc25124e6e9855818d0e9820b9da4a2d85181
Aleksey Fedukin <alekseyfedukin@gmail.com>
2020-01-01 13:03:11 +0200

    Add function drawGame()

--- js/game.js
+++ js/game.js
@@ -1,2 +1,16 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
+
+const ground = new Image();
+ground.src = "img/background.png";
+
+const food = new Image();
+food.src = "img/carrot.png";
+
+let box = 32;
+
+let score = 0;
+
+function drawGame() {
+    ctx.drawImage(background, 0, 0);
+}
