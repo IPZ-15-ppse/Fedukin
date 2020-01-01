a0be7289a64b1b97732ccad209417056a8c76de3
Aleksey Fedukin <alekseyfedukin@gmail.com>
2020-01-01 23:22:28 +0200

    Text Color

--- js/game.js
+++ js/game.js
@@ -4,8 +4,8 @@ const ctx = canvas.getContext("2d");
 const ground = new Image();
 ground.src = "img/ground.png";
 
-const food = new Image();
-food.src = "img/food.png";
+const foodImg = new Image();
+foodImg.src = "img/food.png";
 
 let box = 32;
 
@@ -13,11 +13,26 @@ let score = 0;
 
 let food = {
   x: Msth.floor((Math.random() * 17 + 1)) * box,
-  y: Msth.floor((Math.random() * 15 + 3)) * box,
+  y: Msth.floor((Math.random() * 15 + 1)) * box,
 };
+
+let snake = [];
+snake[0] = {
+	x: 9 * box;
+	y: 10 * box;
+}
 
 function drawGame() {
     ctx.drawImage(background, 0, 0);
+
+	ctx.drawImage(foodImg, food.x, food.y);
+
+	for(let i = 0; i< snake.length; i++) {
+		ctx.fillStyle = "black";
+		ctx.fillRect(snake[i].x, snake[i].y, box, box);
+	}
+
+	ctx.fillStyle = "white";
 }
 
 let game = setInterval(drawGame, 100);
