026f7dab4cfb5df5099ef649da3c866b535a69bb
Aleksey Fedukin <alekseyfedukin@gmail.com>
2020-01-02 00:01:19 +0200

    Snake move and grow

--- js/game.js
+++ js/game.js
@@ -31,13 +31,39 @@ function drawGame() {
 		ctx.fillStyle = "black";
 		ctx.fillRect(snake[i].x, snake[i].y, box, box);
 	}
-<<<<<<< HEAD
-=======
 
 	ctx.fillStyle = "white";
 	ctx.font = "50px Arial";
-	ctx.fillText(score, box * 2.5, box * 1.5);
->>>>>>> lab3.2
+	ctx.fillText(score, box * 2.5, box * 1.7);
+
+	let snakeX = snake[0].x;
+  	let snakeY = snake[0].y;
+
+	if(snakeX == food.x && snakeY == food.y) {
+    score++;
+    food = {
+      x: Math.floor((Math.random() * 17 + 1)) * box,
+      y: Math.floor((Math.random() * 15 + 3)) * box,
+    };
+  } else {
+    snake.pop();
+  }
+
+  if(snakeX < box || snakeX > box * 17
+    || snakeY < 3 * box || snakeY > box * 17)
+    clearInterval(game);
+
+  if(dir == "left") snakeX -= box;
+  if(dir == "right") snakeX += box;
+  if(dir == "up") snakeY -= box;
+  if(dir == "down") snakeY += box;
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  };
+
+
 }
 
 let game = setInterval(drawGame, 100);
